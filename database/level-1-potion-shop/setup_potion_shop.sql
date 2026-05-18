b8ae6a02e098b514114cce830274ce7843b54151
iNullPointer <marianaporto1999@gmail.com>
2026-05-18 09:07:49 +0100

    Update setup_potion_shop.sql

--- database/level-1-potion-shop/setup_potion_shop.sql
+++ database/level-1-potion-shop/setup_potion_shop.sql
@@ -4,92 +4,109 @@
 -- DESCRIPTION: PostgreSQL script for table creation, constraints, 
 --              data population, and relational testing.
 -- ============================================================================
-
 -- ----------------------------------------------------------------------------
 -- 1. CLEANUP (Drop tables in reverse order of dependencies if they exist)
 -- ----------------------------------------------------------------------------
 DROP TABLE IF EXISTS orders;
-DROP TABLE IF EXISTS customers;
-DROP TABLE IF EXISTS potions;
 
+DROP TABLE IF EXISTS customers;
+
+DROP TABLE IF EXISTS potions;
 -- ----------------------------------------------------------------------------
 -- 2. DATABASE STRUCTURE (DDL)
 -- ----------------------------------------------------------------------------
-
-
 -- Potions - The Inventory
-create table potions (
-	potion_id SERIAL primary key,
-	name TEXT not null,
-	price DECIMAL (10,2) not null,
-	stock INT not null
+CREATE TABLE potions (
+	potion_id SERIAL PRIMARY KEY,
+	name TEXT NOT NULL,
+	price DECIMAL (10,
+2) NOT NULL,
+	stock INT NOT NULL
 );
-
 -- Customers - The Heroes
-create table customers (
-	customer_id SERIAL primary key,
-	hero_name TEXT not null,
-	class TEXT not null,
-	is_alive BOOLEAN default true
+CREATE TABLE customers (
+	customer_id SERIAL PRIMARY KEY,
+	hero_name TEXT NOT NULL,
+	CLASS TEXT NOT NULL,
+	is_alive BOOLEAN DEFAULT TRUE
 );
-
 -- Orders - The Sales History
-create table orders (
-	order_id SERIAL primary key,
-	potion_id INT not null,
-	customer_id INT not null,
-	quantity INT not null default 1,
-	order_date TIMESTAMP default current_TIMESTAMP,
-	
-	--FK link to ID
-	constraint fk_potions foreign key (potion_id) references potions(potion_id),
-	constraint fk_customers foreign key (customer_id) references customers(customer_id)
+CREATE TABLE orders (
+	order_id SERIAL PRIMARY KEY,
+	potion_id INT NOT NULL,
+	customer_id INT NOT NULL,
+	quantity INT NOT NULL DEFAULT 1,
+	order_date TIMESTAMP DEFAULT current_TIMESTAMP,
+--FK link to ID
+	CONSTRAINT fk_potions FOREIGN KEY (potion_id) REFERENCES potions(potion_id),
+	CONSTRAINT fk_customers FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
 	);
-
-
 -- ----------------------------------------------------------------------------
 -- 3. DATA POPULATION (DML)
 -- ----------------------------------------------------------------------------
-
 -- Populating Potion Inventory 
-insert into potions (name, price, stock) values
+INSERT
+	INTO
+	potions (name,
+	price,
+	stock)
+VALUES
 ('Health Elixir', 12.50, 15),
 ('Mana Potion', 15.00, 20),
 ('Poison Antidote', 8.99, 10),
 ('Fire Resistance', 11.75, 10),
 ('Invisibility', 15.00, 20);
-
 -- Populating Customers (The Heroes)
-insert into customers (hero_name, class, is_alive) values
-('Mario', 'Warrior', true),
-('Alvo', 'Mage', false),
-('Frodo', 'Healer', true),
-('Dean', 'Hunter', true),
-('Vecna', 'Mage', false);
-
+INSERT
+	INTO
+	customers (hero_name,
+	CLASS,
+	is_alive)
+VALUES
+('Mario', 'Warrior', TRUE),
+('Alvo', 'Mage', FALSE),
+('Frodo', 'Healer', TRUE),
+('Dean', 'Hunter', TRUE),
+('Vecna', 'Mage', FALSE);
 -- Registering Orders (Sales)
-insert into orders (potion_id, customer_id, quantity) values
+INSERT
+	INTO
+	orders (potion_id,
+	customer_id,
+	quantity)
+VALUES
 (1, 1, 2),
 (2, 2, 1),
 (5, 3, 5),
 (3, 4, 3),
 (4, 5, 2);
-
-
 -- ----------------------------------------------------------------------------
 -- 4. VERIFICATION / TESTING (Queries)
 -- ----------------------------------------------------------------------------
-
 -- Relational query to generate a complete sales report with names (JOIN)
-select * from potions order by price asc;
+SELECT
+	*
+FROM
+	potions
+ORDER BY
+	price DESC;
 
-select hero_name, class from customers where is_alive = false;
+SELECT
+	hero_name,
+	CLASS
+FROM
+	customers
+WHERE
+	is_alive = FALSE;
 
-select
-	c.hero_name as hero,
-	p.name as potion_name,
-	o.quantity as quantity,
-	o.order_date as date_and_hour
-from orders o
-join customers c on o.customer_id = c.customer_id
-join potions p on o.potion_id = p.potion_id;
+SELECT
+	c.hero_name AS hero,
+	p.name AS potion_name,
+	o.quantity AS quantity,
+	o.order_date AS date_and_hour
+FROM
+	orders o
+JOIN customers c ON
+	o.customer_id = c.customer_id
+JOIN potions p ON
+	o.potion_id = p.potion_id;
